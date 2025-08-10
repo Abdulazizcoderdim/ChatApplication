797ab66c30b18ee7727faaf925897ad1fe94dd84
AbdulazizRus <coderdim@gmail.com>
2025-08-10 09:42:23 +0500

    Add react-icons dependency and implement chat functionality with message handling, user contacts, and emoji support in ChatContainer and ChatInput components.

--- public/src/pages/Chat.tsx
+++ public/src/pages/Chat.tsx
@@ -1,5 +1,94 @@
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { io, Socket } from "socket.io-client";
+import { allUsersRoute, host } from "../utils/api-route";
+import type { IUser } from "../types";
+import styled from "styled-components";
+import axios from "axios";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+import ChatContainer from "../components/ChatContainer";
+
 const Chat = () => {
-  return <div>Chat</div>;
+  const navigate = useNavigate();
+  const socket = useRef<Socket | null>(null);
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState<IUser | undefined>(undefined);
+  const [currentUser, setCurrentUser] = useState<IUser | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = localStorage.getItem(import.meta.env.VITE_LOCALHOST_KEY);
+      if (!userData) {
+        navigate("/login");
+      } else {
+        setCurrentUser(JSON.parse(userData));
+      }
+    };
+
+    fetchUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser?._id);
+    }
+  }, [currentUser]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    };
+  }, [currentUser]);
+
+  const handleChatChange = (chat: IUser) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+
+          {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
+  );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat;
