5d2bb3047834e311ae9f05f5f6d573e931e1e8a7
AbdulazizRus <coderdim@gmail.com>
2025-08-10 12:28:47 +0500

    Enhance Welcome component to display personalized greeting by fetching username from local storage. Add styled container for improved layout and visual appeal.

--- public/src/components/Welcome.tsx
+++ public/src/components/Welcome.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setUserName(
+        await JSON.parse(
+          localStorage.getItem(import.meta.env.VITE_LOCALSTORAGE_KEY) || "{}"
+        ).username
+      );
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <img src="/robot.gif" alt="robot" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
 };
 
 export default Welcome;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
